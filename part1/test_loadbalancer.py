27b91a664654c50833f8980a6a0370960431a049
Neeran Gul <paktek123@gmail.com>
2020-03-27 00:41:42 +0000

    Update test_loadbalancer.py

--- part1/test_loadbalancer.py
+++ part1/test_loadbalancer.py
@@ -7,6 +7,6 @@ def client():
     with loadbalancer.test_client() as client:
         yield client
 
-def test_not_found(client):
+def test_hello(client):
     result = client.get('/')
     assert b'hello' in result.data
